2ead4475a05b3fb2c5516925ef854cc6cd97321e
Natalia Braz <naabraz@gmail.com>
2022-12-01 15:21:40 -0300

    chore: add react navigation stack

--- demo-modules/demo-module-a/module.js
+++ demo-modules/demo-module-a/module.js
@@ -1,23 +1,50 @@
 import React from 'react';
 import { Alert } from 'react-native';
 
-import { ThemeProvider, Box, Button, Text } from '@gympass/yoga';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ThemeProvider, Box, Text, Button } from '@gympass/yoga';
+
+function HomeScreen({ navigation }) {
+  return (
+    <Box flex={1} alignItems="center" justifyContent="center" bg="neutral">
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </Box>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <Box flex={1} alignItems="center" justifyContent="center" bg="neutral">
+      <Text>Details Screen</Text>
+      <Button.Link
+        marginTop="small"
+        onPress={() =>
+          engine.moduleRegistry.invoke('demo-module-b.some-method')
+        }>
+        Say Hello to module B
+      </Button.Link>
+    </Box>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 class Screen extends React.Component {
   render() {
     return (
-      <ThemeProvider>
-        <Box flex={1} alignItems="center" justifyContent="center" bg="neutral">
-          <Text>Demo Module A</Text>
-          <Button.Link
-            marginTop="small"
-            onPress={() =>
-              engine.moduleRegistry.invoke('demo-module-b.some-method')
-            }>
-            Say Hello to module B
-          </Button.Link>
-        </Box>
-      </ThemeProvider>
+      <NavigationContainer>
+        <ThemeProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+          </Stack.Navigator>
+        </ThemeProvider>
+      </NavigationContainer>
     );
   }
 }
